7eea697aa381454113883ebca20f8ff342e9ffe7
Toni <toni.asikainen@icloud.com>
2020-08-28 10:10:27 +0300

    Initial Commit

---
+++ gittest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittest2
+//
+//  Created by Toni on 28.8.2020.
+//  Copyright © 2020 Toni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
